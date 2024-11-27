d4af9d016d4e41059e314b10a4143570323dbd52
donghao0210 <donghao20082001@gmail.com>
2024-11-27 16:11:30 +0800

    fix: Dockerfile of backend

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
       <h1>API Demo</h1>
+      <p>App Version: {{ version }}</p>
       <button @click="fetchHello">Get Hello</button>
       <button @click="fetchGreet">Get Greet</button>
       <p v-if="url">Request made to: {{ url }}</p>
@@ -14,6 +15,7 @@ import api from '../api';
 export default {
   data() {
       return {
+          version: process.env.VUE_APP_VERSION,
           url: '', // To store the URL of the request
           response: '', // To store the response or error message
       };
